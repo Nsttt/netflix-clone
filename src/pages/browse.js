a9b56183ea7bc7b9fd9f3f1c9b7fdc1881124556
Nestor <elnestinatorHD@gmail.com>
2020-10-01 19:30:55 +0200

    Added custom hook for content.

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+  // Stories and Films.
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  // Slideshare.
+
   return <p>test</p>;
 }
