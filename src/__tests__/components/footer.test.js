de685416fe6e9b275e5488786eec111b405bc361
Nestor <elnestinatorHD@gmail.com>
2020-10-12 19:40:31 +0200

    Starting working around tests. Needs more work.

---
+++ src/__tests__/components/footer.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Footer } from '../../components';
+
+describe('<Footer />', () => {
+  it('renders the <Footer /> with populated data', () => {
+    const { container, getByText } = render(
+      <Footer>
+        <Footer.Title>Questions ? Contact us.</Footer.Title>
+        <Footer.Break />
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">FAQ</Footer.Link>
+            <Footer.Link href="#">Investor Relations</Footer.Link>
+            <Footer.Link href="#">Ways to watch</Footer.Link>
+            <Footer.Link href="#">Corporate Information</Footer.Link>
+            <Footer.Link href="#">Originals</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Help Center</Footer.Link>
+            <Footer.Link href="#">Jobs</Footer.Link>
+            <Footer.Link href="#">Terms of use</Footer.Link>
+            <Footer.Link href="#">Contact us</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Account</Footer.Link>
+            <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
+            <Footer.Link href="#">Privacy</Footer.Link>
+            <Footer.Link href="#">Speed test</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Media Centre</Footer.Link>
+            <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+            <Footer.Link href="#">Cookie Preferences</Footer.Link>
+            <Footer.Link href="#">Legal Notices</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+        <Footer.Break />
+        <Footer.Text>Netflix Spain</Footer.Text>
+      </Footer>
+    );
+
+    expect(getByText('Questions ? Contact us.')).toBeTruthy();
+    expect(getByText('FAQ')).toBeTruthy();
+    expect(getByText('Investor Relations')).toBeTruthy();
+    expect(getByText('Ways to watch')).toBeTruthy();
+    expect(getByText('Corporate Information')).toBeTruthy();
+    expect(getByText('Originals')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
